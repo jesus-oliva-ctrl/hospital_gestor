d62879e9265346d50cbeb91bcb27c50d09c5c94e
Benjamin <jesus.oliva@ucb.edu.bo>
2025-11-18 09:17:26 -0400

    Update Project

---
+++ HospitalWeb/Program.cs
@@ -0,0 +1,45 @@
+using HospitalWeb.Components;
+using HospitalData.Models;
+using Microsoft.EntityFrameworkCore;
+using HospitalData.Services;
+using HospitalData.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<HospitalDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HospitalDB"))
+);
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddSingleton<UserSessionService>();
+
+builder.Services.AddScoped<IStaffService, StaffService>();
+
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host"); 
+
+app.Run();
